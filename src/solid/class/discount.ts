df14446606914a468aefd70a83ba6954512268c9
Bruno <brunosslima@yahoo.com.br>
2025-01-15 15:37:24 -0300

    Refactoring discount.ts

--- src/solid/class/discount.ts
+++ src/solid/class/discount.ts
@@ -1,25 +1,17 @@
 export abstract class Discount {
-  abstract calculate(totalPrice: number): number;
+  protected discount = 0;
+
+  calculate(totalPrice: number): number {
+    return totalPrice - totalPrice * (this.discount / 100);
+  }
 }
 
 export class FivePercentDiscount extends Discount {
-  private readonly discount = 5;
-
-  calculate(totalPrice: number): number {
-    return totalPrice - totalPrice * (this.discount / 100);
-  }
+  protected readonly discount = 5;
 }
 
 export class TenPercentDiscount extends Discount {
-  private readonly discount = 10;
-
-  calculate(totalPrice: number): number {
-    return totalPrice - totalPrice * (this.discount / 100);
-  }
+  protected readonly discount = 10;
 }
 
-export class NoDiscount extends Discount {
-  calculate(totalPrice: number): number {
-    return totalPrice;
-  }
-}
+export class NoDiscount extends Discount {}
